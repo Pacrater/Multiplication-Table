18dd6a211435a92ce6e85f4dd20832747a9c0977
Oshekher <donavon.haag@my.tccd.edu>
2019-11-12 17:02:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+print("TABLE OF 1")
+
+i = 1
+while i <= 10:
+	print(i)
+	i = i + 1
+
+print("\nfinished\n")
+
+print("TABLE OF 2")
+
+i = 2
+while i <= 20:
+	print(i)
+	i = i + 2
+
+print("\nfinished\n")
+
+print("TABLE OF 3")
+
+i = 3
+while i <= 30:
+	print(i)
+	i = i + 3
+
+print("\nfinidhed\n")
+
+print("TABLE OF 4")
+
+i = 4
+while i <= 40:
+	print(i)
+	i = i + 4
